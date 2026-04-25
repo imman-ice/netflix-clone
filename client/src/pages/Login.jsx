8b3b31dc33c89e3755e6fe37d68daa51e2d29ed4
imman-ice <immansamuel52@gmail.com>
2026-04-25 21:07:23 +0530

    Updated API URL to Render backend

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -23,7 +23,7 @@ export default function Login() {
     try {
       setLoading(true);
 
-      const res = await axios.post("http://localhost:5000/api/login", {
+      axios.post("https://netflix-clone-p9fy.onrender.com/api/login", {
         email,
         password,
       });
